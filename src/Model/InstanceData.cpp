05415add9afc600d5b5a8b552174e7bf19363bb1
Akira-Hayasaka <akira.hayasaka@gmail.com>
2015-04-30 17:16:53 +0900

    refactoring

--- src/Model/InstanceData.cpp
+++ src/Model/InstanceData.cpp
@@ -12,28 +12,37 @@ void InstanceData::setup(int idx)
 {
     this->idx = idx;
     animPct = ofRandom(1.0);
+    
+    int xbegin = -500;
+    
+    int zbegin = -500;
+    if (idx % 2 == 0)
+        zbegin = -550;
+    pos.x = xbegin + (idx * 5);
+    pos.y = -15;
+    pos.z = zbegin + (idx * 5);
 }
 
 void InstanceData::update()
 {
-    float t = Globals::ELAPSED_TIME;
-    
-    pos.x = ofNoise(0.001 * t, idx + 0) * 100 -50;
-    pos.y = ofNoise(0.001 * t, idx + 10) * 100 -50;
-    pos.z = ofNoise(0.001 * t, idx + 20) * 100 -50;
-    
-    float angle = ofNoise(0.05 * t, idx + 60) * 360;
-    
-    axis.x = ofNoise(0.02 * t, idx + 30);
-    axis.y = ofNoise(0.02 * t, idx + 40);
-    axis.z = ofNoise(0.02 * t, idx + 50);
-    
-    float scale = ofNoise(0.02 * t, idx + 70) * 2.0 + 0.5;
-    
+//    float t = Globals::ELAPSED_TIME;
+//    
+//    pos.x = ofNoise(0.001 * t, idx + 0) * 100 -50;
+//    pos.y = ofNoise(0.001 * t, idx + 10) * 100 -50;
+//    pos.z = ofNoise(0.001 * t, idx + 20) * 100 -50;
+//    
+//    float angle = ofNoise(0.05 * t, idx + 60) * 360;
+//    
+//    axis.x = ofNoise(0.02 * t, idx + 30);
+//    axis.y = ofNoise(0.02 * t, idx + 40);
+//    axis.z = ofNoise(0.02 * t, idx + 50);
+//    
+//    float scale = ofNoise(0.02 * t, idx + 70) * 2.0 + 0.5;
+//    
     mat = ofMatrix4x4::newTranslationMatrix(ofVec3f());
-    
-    mat.rotate(angle, axis.x, axis.y, axis.z);
-    mat.scale(scale, scale, scale);
+//
+//    mat.rotate(angle, axis.x, axis.y, axis.z);
+//    mat.scale(scale, scale, scale);
     mat.translate(pos);
     
     animPct += 0.015;
